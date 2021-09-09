8091c3c0ddf49ebf5433db0b3edafa6ce32bf143
Ilya Vlasov <ilyavlasov2011@gmail.com>
2021-09-09 23:45:48 +0300

    Improved split and replaced float with Decimal

--- src/common/model/LineSegment.py
+++ src/common/model/LineSegment.py
@@ -1,25 +1,39 @@
 from dataclasses import dataclass
-from typing import Tuple
+from decimal import Decimal
+from typing import List, Union
 
 
-@dataclass(frozen=True)
+@dataclass(init=False)
 class LineSegment:
-    left: float
-    right: float
+    left: Decimal
+    right: Decimal
 
-    def __post_init__(self):
+    def __init__(self, left: Union[float, Decimal], right: Union[float, Decimal]):
+        self.left = Decimal(left)
+        self.right = Decimal(right)
+
         if self.left > self.right:
             raise ValueError(f'The start of the segment should not exceed its end ({self.left} > {self.right}).')
 
     def __str__(self) -> str:
         return f'[{self.left}, {self.right}]'
 
-    def __len__(self) -> float:
-        return self.right - self.left
+    def split(self, parts: int = 2) -> List['LineSegment']:
+        part_length = self.length / parts
 
-    def split(self) -> Tuple['LineSegment', 'LineSegment']:
-        return LineSegment(self.left, self.midpoint), LineSegment(self.midpoint, self.right)
+        segments = []
+        current_left = self.left
+        for _ in range(parts):
+            current_right = current_left + part_length
+            segments.append(LineSegment(current_left, current_right))
+            current_left = current_right
+
+        return segments
 
     @property
-    def midpoint(self) -> float:
+    def length(self) -> Decimal:
+        return self.right - self.left
+
+    @property
+    def midpoint(self) -> Decimal:
         return (self.left + self.right) / 2
