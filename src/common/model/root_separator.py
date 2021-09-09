887e85683979277556844f2fc061fb1586de05fd
Ilya Vlasov <ilyavlasov2011@gmail.com>
2021-09-10 00:15:15 +0300

    Triggered GA

--- src/common/model/root_separator.py
+++ src/common/model/root_separator.py
@@ -6,5 +6,5 @@ from src.common.model.line_segment import LineSegment
 
 class RootSeparator(ABC):
     @abstractmethod
-    def separate(self, line_segment: LineSegment, function, variable: str = 'x') -> List[LineSegment]:  # noqa: WPS110
+    def separate(self, line_segment: LineSegment, function, variable: str = 'x') -> List[LineSegment]:
         raise NotImplementedError
