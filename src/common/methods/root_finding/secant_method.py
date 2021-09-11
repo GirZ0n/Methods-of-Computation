8f2f5da32e9978d77bdeaa10c186057f30a74cf7
Ilya Vlasov <ilyavlasov2011@gmail.com>
2021-09-11 19:59:28 +0300

    Added secant method

---
+++ src/common/methods/root_finding/secant_method.py
@@ -0,0 +1,41 @@
+from typing import Optional
+
+from sympy import lambdify, parse_expr
+
+from src.common.model.line_segment import LineSegment
+from src.common.model.root_finder import RootFinder, RootFinderStats
+
+
+class SecantMethod(RootFinder):
+    stats: Optional[RootFinderStats]
+
+    def __init__(self):
+        self.stats = None
+
+    def find(self, *, expression, variable: str = 'x', line_segment: LineSegment, accuracy: float) -> Optional[float]:
+        f = lambdify(variable, expression)
+
+        previous_x = line_segment.left
+        current_x = line_segment.right
+        next_x = current_x - (f(current_x) / (f(current_x) - f(previous_x))) * (current_x - previous_x)
+
+        current_step = 1
+        while abs(next_x - current_x) > accuracy:
+            previous_x = current_x
+            current_x = next_x
+            next_x = current_x - (f(current_x) / (f(current_x) - f(previous_x))) * (current_x - previous_x)
+
+            current_step += 1
+
+        approximate_solution = next_x
+
+        self.stats = RootFinderStats(
+            line_segment=line_segment,
+            initial_approximation=line_segment.left,
+            number_of_steps=current_step,
+            approximate_solution=approximate_solution,
+            error=abs(approximate_solution - current_x),
+            residual=f(approximate_solution),
+        )
+
+        return approximate_solution
