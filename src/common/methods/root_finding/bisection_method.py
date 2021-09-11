d7bcb5728482c4a6725c89d3cb65426dd0f40754
Ilya Vlasov <ilyavlasov2011@gmail.com>
2021-09-11 19:41:12 +0300

    Small fixes

--- src/common/methods/root_finding/bisection_method.py
+++ src/common/methods/root_finding/bisection_method.py
@@ -13,24 +13,19 @@ class BisectionMethod(RootFinder):
         self.stats = None
 
     def find(self, *, expression, variable: str = 'x', line_segment: LineSegment, accuracy: float) -> Optional[float]:
-        self.stats = RootFinderStats()
-
         f = lambdify(variable, expression)
 
         current_segment = line_segment
         current_step = 0
-        current_x = current_segment.midpoint
         while current_segment.length > 2 * accuracy:
-            current_x = current_segment.midpoint
-
             left_value = f(current_segment.left)
-            midpoint_value = f(current_x)
+            midpoint_value = f(current_segment.midpoint)
             right_value = f(current_segment.right)
 
             if left_value * midpoint_value <= 0:
-                current_segment = LineSegment(current_segment.left, current_x)
+                current_segment = LineSegment(current_segment.left, current_segment.midpoint)
             elif midpoint_value * right_value <= 0:
-                current_segment = LineSegment(current_x, current_segment.right)
+                current_segment = LineSegment(current_segment.midpoint, current_segment.right)
             else:
                 return None
 
@@ -38,11 +33,13 @@ class BisectionMethod(RootFinder):
 
         approximate_solution = current_segment.midpoint
 
-        self.stats.line_segment = line_segment
-        self.stats.initial_approximation = line_segment.midpoint
-        self.stats.number_of_steps = current_step
-        self.stats.approximate_solution = approximate_solution
-        self.stats.error = abs(approximate_solution - current_x)
-        self.stats.residual = f(approximate_solution)
+        self.stats = RootFinderStats(
+            line_segment=line_segment,
+            initial_approximation=line_segment.midpoint,
+            number_of_steps=current_step,
+            approximate_solution=approximate_solution,
+            error=current_segment.length,
+            residual=f(approximate_solution),
+        )
 
-        return current_segment.midpoint
+        return approximate_solution
