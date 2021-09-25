9bdd174e78fa8e390cbeea62949df55c401ff1fd
Ilya Vlasov <ilyavlasov2011@gmail.com>
2021-09-25 22:19:00 +0300

    Added sample_floats

---
+++ src/utils.py
@@ -0,0 +1,15 @@
+import random
+from typing import List
+
+
+def sample_floats(low: float, high: float, k: int = 1) -> List[float]:
+    """Return a k-length list of unique random floats in the range of low <= x <= high."""
+    seen = set()
+    for _ in range(k):
+        x = random.uniform(low, high)
+        while x in seen:
+            x = random.uniform(low, high)
+
+        seen.add(x)
+
+    return list(seen)
