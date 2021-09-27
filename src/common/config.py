81daaf68cb84099e67263f6c7cf5009c88f3bc7a
Ilya Vlasov <ilyavlasov2011@gmail.com>
2021-09-27 22:13:25 +0300

    Added new fields

--- src/common/config.py
+++ src/common/config.py
@@ -1,1 +1,22 @@
+from sympy.parsing.sympy_parser import (
+    convert_xor,
+    function_exponentiation,
+    implicit_application,
+    implicit_multiplication,
+    split_symbols,
+    standard_transformations,
+)
+
 OUTPUT_PRECISION = 10
+
+TRANSFORMATIONS = standard_transformations + (
+    split_symbols,
+    implicit_application,
+    implicit_multiplication,
+    function_exponentiation,
+    convert_xor,
+)
+
+MAIN_COLOR = '#FF2B68'
+SECOND_COLOR = '#5E5698'
+LIGHT_GRAY = '#d3d3d3'
