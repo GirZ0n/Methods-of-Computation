534df9d6a499e9573491527e90bca7e6537c2cc0
Ilya Vlasov <ilyavlasov2011@gmail.com>
2021-11-03 12:41:23 +0300

    Small fix

--- src/tasks/task4/subtask1/fragments/sidebar.py
+++ src/tasks/task4/subtask1/fragments/sidebar.py
@@ -10,10 +10,10 @@ def show_sidebar():
         text_expression = st.text_input('Выражение:', value='x^3 + x^2 + 1')
         StateVar.TEXT_EXPRESSION.set(text_expression)
 
-        a = int(st.number_input('A:', value=1))
+        a = st.number_input('A:', value=float(1))
         StateVar.LEFT_BOUNDARY.set(a)
 
-        b = int(st.number_input('B:', value=3))
+        b = st.number_input('B:', value=float(3))
         StateVar.RIGHT_BOUNDARY.set(b)
 
         n = int(st.number_input('Количество частей:', value=3, min_value=1))
