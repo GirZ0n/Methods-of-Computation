f614f5cbc09259281ff6fa533a2726d0f8c66e76
Ilya Vlasov <ilyavlasov2011@gmail.com>
2021-11-13 18:25:10 +0300

    Add optimized method

--- src/common/methods/numerical_integration/rectangle_methods.py
+++ src/common/methods/numerical_integration/rectangle_methods.py
@@ -34,3 +34,47 @@ class TrapezoidalMethod(NumericalIntegrator):
     def integrate(self, *, f: Callable, segment: LineSegment, n: int, **kwargs) -> float:
         points = segment.split_into_points(n)
         return sum((f(x_left) + f(x_right)) * (x_right - x_left) / 2 for x_left, x_right in zip(points, points[1:]))
+
+
+# ----------------------------------------------------------------------------------------------------------------------
+
+
+class OptimizedLeftRectangleMethod(NumericalIntegrator):
+    accuracy_degree = 0
+
+    def integrate(self, *, f: Callable, segment: LineSegment, n: int, **kwargs) -> float:
+        inner_sum = kwargs.pop('inner_sum')
+        h = segment.length / n
+
+        return h * (f(segment.left) + inner_sum)
+
+
+class OptimizedRightRectangleMethod(NumericalIntegrator):
+    accuracy_degree = 0
+
+    def integrate(self, *, f: Callable, segment: LineSegment, n: int, **kwargs) -> float:
+        inner_sum = kwargs.pop('inner_sum')
+        h = segment.length / n
+
+        return h * (inner_sum + f(segment.right))
+
+
+class OptimizedMiddleRectangleMethod(NumericalIntegrator):
+    accuracy_degree = 1
+
+    def integrate(self, *, f: Callable, segment: LineSegment, n: int, **kwargs) -> float:
+        middle_sum = kwargs.pop('middle_sum')
+        h = segment.length / n
+
+        return h * middle_sum
+
+
+class OptimizedTrapezoidalMethod(NumericalIntegrator):
+    accuracy_degree = 1
+
+    def integrate(self, *, f: Callable, segment: LineSegment, n: int, **kwargs) -> float:
+        inner_sum = kwargs.pop('inner_sum')
+        boundary_sum = kwargs.pop('boundary_sum')
+        h = segment.length / n
+
+        return h * (inner_sum + boundary_sum / 2)
