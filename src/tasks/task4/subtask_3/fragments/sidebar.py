0fc5f48d49e8b45e05f8680d1a7a94f294e6874c
Ilya Vlasov <ilyavlasov2011@gmail.com>
2021-11-15 21:40:18 +0300

    Added task 4.3

---
+++ src/tasks/task4/subtask_3/fragments/sidebar.py
@@ -0,0 +1,23 @@
+import streamlit as st
+
+from src.tasks.task4.subtask_3.common.state_var import StateVar
+
+
+def show_sidebar():
+    with st.sidebar:
+        st.title('Параметры задачи')
+
+        function = st.text_input('f(x):', value='x^3 + x^2 + 1')
+        StateVar.FUNCTION.set(function)
+
+        a = st.number_input('A:', value=float(1))
+        StateVar.LEFT_BOUNDARY.set(a)
+
+        b = st.number_input('B:', value=float(3))
+        StateVar.RIGHT_BOUNDARY.set(b)
+
+        n = int(st.number_input('Количество частей:', value=40, min_value=1))
+        StateVar.NUMBER_OF_SEGMENTS.set(n)
+
+        l = int(st.number_input('Множитель l:', value=3, min_value=1))  # noqa: E741
+        StateVar.MULTIPLIER.set(l)
