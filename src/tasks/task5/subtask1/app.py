3a07403ecf3ab0aae9d468c0a6b4eb14fb39525b
Ilya Vlasov <ilyavlasov2011@gmail.com>
2021-12-04 18:11:25 +0300

    Code refactoring

--- src/tasks/task5/subtask1/app.py
+++ src/tasks/task5/subtask1/app.py
@@ -1,38 +1,37 @@
+from typing import Callable, List
+
 import pandas as pd
 import streamlit as st
 from sympy import integrate, lambdify, parse_expr
 
 from src.common.consts import TRANSFORMATIONS
-from src.common.methods.numerical_integration.gaussian_method import GaussianMethod, get_coefficients, get_roots
+from src.common.methods.numerical_integration.gaussian_method import (
+    GaussianMethod,
+    get_gaussian_coefficients,
+    get_gaussian_roots,
+)
+from src.common.methods.numerical_integration.mohler_method import (
+    MohlerMethod,
+    get_mohler_coefficients,
+    get_mohler_roots,
+)
 from src.common.model.line_segment import LineSegment
+from src.common.model.numerical_integrator import NumericalIntegrator
 from src.tasks.task5.subtask1.common.state_var import StateVar
 from src.tasks.task5.subtask1.fragments.sidebar import show_sidebar
 
 MAXIMUM_NUMBER_OF_NODES = 8
 
 
-def _table_to_latex(table: pd.DataFrame, precision: int) -> str:
-    styler = table.style
-    styler.format(precision=precision)
-    return styler.to_latex(column_format='c' * len(table.columns), hrules=True)
-
-
-def _latex_to_katex(latex_str: str) -> str:
-    katex_str = r'\def\arraystretch{1.3}' + latex_str  # noqa: WPS336
-    katex_str = katex_str.replace('tabular', 'array')
-    katex_str = katex_str.replace('toprule', 'hline')
-    katex_str = katex_str.replace('midrule', 'hline')
-    katex_str = katex_str.replace('bottomrule', 'hline')
-    return katex_str  # noqa: WPS331
-
-
-def _show_results(number_of_nodes: int) -> None:
+def _show_results(
+    f: Callable,
+    precise_solution: float,
+    number_of_nodes: int,
+    roots: List[float],
+    coefficients: List[float],
+    method: NumericalIntegrator,
+) -> None:
     st.subheader(f'Узлов: {number_of_nodes}')
-
-    segment = LineSegment(StateVar.LEFT_BOUNDARY.get(), StateVar.RIGHT_BOUNDARY.get())
-
-    roots = get_roots(number_of_nodes, segment)
-    coefficients = get_coefficients(number_of_nodes, segment)
 
     if StateVar.SHOW_TABLE.get():
         df = pd.DataFrame({'Корни': roots, 'Коэффициенты': coefficients})
@@ -43,15 +42,8 @@ def _show_results(number_of_nodes: int) -> None:
 
         st.dataframe(styler)
 
-        st.write(fr'$\sum\limits_{{k = 0}}^{{{number_of_nodes - 1}}} a_k = {sum(coefficients)} = B - A$')
+        st.write(fr'$\sum\limits_{{k = 0}}^{{{number_of_nodes - 1}}} a_k = {sum(coefficients)}$')
 
-    f_expression = parse_expr(StateVar.FUNCTION.get(), transformations=TRANSFORMATIONS)
-    f = lambdify('x', f_expression)
-    precise_solution = float(
-        integrate(f_expression, ('x', StateVar.LEFT_BOUNDARY.get(), StateVar.RIGHT_BOUNDARY.get())),
-    )
-
-    method = GaussianMethod()
     approximate_solution = method.integrate(
         f=f,
         segment=LineSegment(StateVar.LEFT_BOUNDARY.get(), StateVar.RIGHT_BOUNDARY.get()),
@@ -69,24 +61,69 @@ def _show_results(number_of_nodes: int) -> None:
         )
 
 
+def show_gaussian_method() -> None:
+    nodes = st.multiselect(
+        'Количество узлов:',
+        options=range(1, StateVar.MAX_NUMBER_OF_NODES.get() + 1),
+    )
+
+    segment = LineSegment(StateVar.LEFT_BOUNDARY.get(), StateVar.RIGHT_BOUNDARY.get())
+
+    f_expression = parse_expr(StateVar.FUNCTION.get(), transformations=TRANSFORMATIONS)
+    f = lambdify('x', f_expression)
+
+    precise_solution = float(
+        integrate(f_expression, ('x', StateVar.LEFT_BOUNDARY.get(), StateVar.RIGHT_BOUNDARY.get())),
+    )
+
+    for number_of_nodes in nodes:
+        roots = get_gaussian_roots(number_of_nodes, segment)
+        coefficients = get_gaussian_coefficients(number_of_nodes, segment)
+        _show_results(f, precise_solution, number_of_nodes, roots, coefficients, GaussianMethod())
+
+
+def show_mohler_method() -> None:
+    nodes = st.multiselect(
+        'Количество узлов:',
+        options=range(1, StateVar.MAX_NUMBER_OF_NODES.get() + 1),
+    )
+
+    segment = LineSegment(StateVar.LEFT_BOUNDARY.get(), StateVar.RIGHT_BOUNDARY.get())
+
+    f_expression = parse_expr(StateVar.FUNCTION.get(), transformations=TRANSFORMATIONS)
+    f = lambdify('x', f_expression)
+
+    f_expression_with_weight = parse_expr(
+        f'({StateVar.FUNCTION.get()}) / sqrt(1 - x^2)',
+        transformations=TRANSFORMATIONS,
+    )
+    precise_solution = float(
+        integrate(f_expression_with_weight, ('x', StateVar.LEFT_BOUNDARY.get(), StateVar.RIGHT_BOUNDARY.get())),
+    )
+
+    for number_of_nodes in nodes:
+        roots = get_mohler_roots(number_of_nodes, segment)
+        coefficients = get_mohler_coefficients(number_of_nodes, segment)
+        _show_results(f, precise_solution, number_of_nodes, roots, coefficients, MohlerMethod())
+
+
 def main() -> None:
+    show_sidebar()
+
     st.markdown(
-        """
+        f"""
         <h1 style='text-align: center'>
-            КФ Гаусса, ее узлы и коэффициенты. </br>
-            Вычисление интегралов при помощи КФ Гаусса
+            Вычисление интегралов при помощи КФ {StateVar.METHOD_NAME.get()}.
         </h1>
         """,
         unsafe_allow_html=True,
     )
     st.markdown("<div style='text-align: right'>Вариант 1</div>", unsafe_allow_html=True)
 
-    show_sidebar()
-
-    nodes = st.multiselect('Количество узлов:', options=range(1, MAXIMUM_NUMBER_OF_NODES + 1), default=[2, 5, 6, 8])
-
-    for number_of_nodes in nodes:
-        _show_results(number_of_nodes)
+    if StateVar.METHOD_NAME.get() == 'Гаусса':
+        show_gaussian_method()
+    else:
+        show_mohler_method()
 
 
 if __name__ == '__main__':
