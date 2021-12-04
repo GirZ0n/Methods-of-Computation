9d0ba317c031b83faa29758724608929dd8b3cdb
Ilya Vlasov <ilyavlasov2011@gmail.com>
2021-12-04 12:45:20 +0300

    Small fix

--- src/common/methods/numerical_integration/gaussian_method.py
+++ src/common/methods/numerical_integration/gaussian_method.py
@@ -5,27 +5,27 @@ from src.common.model.line_segment import LineSegment
 from src.common.model.numerical_integrator import NumericalIntegrator
 
 
-def get_roots(n: int) -> List[float]:
-    return LegendrePolynomial(n).get_roots()
+def get_roots(n: int, segment: LineSegment) -> List[float]:
+    roots = LegendrePolynomial(n).get_roots()
+    q = segment.length / 2
+    return list(map(lambda root: segment.left + q * (root + 1), roots))
 
 
-def get_coefficients(n: int) -> List[float]:
+def get_coefficients(n: int, segment: LineSegment) -> List[float]:
     roots = LegendrePolynomial(n).get_roots()
     coefficients = []
     for root in roots:
         coefficients.append(2 * (1 - root ** 2) / (n ** 2 * LegendrePolynomial(n - 1)(root) ** 2))
-    return coefficients
+
+    q = segment.length / 2
+
+    return list(map(lambda coefficient: coefficient * q, coefficients))
 
 
 class GaussianMethod(NumericalIntegrator):
     def integrate(self, *, f: Callable, segment: LineSegment, n: int, **kwargs) -> float:
-        roots = get_roots(n)
-        coefficients = get_coefficients(n)
-
-        q = segment.length / 2
-
-        coefficients = list(map(lambda coefficient: coefficient * q, coefficients))
-        roots = list(map(lambda root: segment.left + q * (root + 1), roots))
+        roots = get_roots(n, segment)
+        coefficients = get_coefficients(n, segment)
 
         return sum([coefficient * f(root) for coefficient, root in zip(coefficients, roots)])
 
